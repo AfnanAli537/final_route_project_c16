81c0db5af026c8b9bd0a34ca558621c20f7d61d0
Afnan Ali <afnan.alial537@gmail.com>
2025-12-06 17:40:02 +0200

    connect browse with home

--- lib/core/network/endpoints.dart
+++ lib/core/network/endpoints.dart
@@ -2,7 +2,7 @@ class Endpoints {
   static const String authBaseUrl = "https://route-movie-apis.vercel.app/";
   static const String moviesBaseUrl = "https://yts.lt/api/v2";
   static const String listMovies = "/list_movies.json";
-  static const String browser = "/list_movies.json?limit=100";
+  static const String browser = "/list_movies.json?limit=50";
   static const String login = "auth/login";
   static const String register = "auth/register";
   static const String resetPassword = "auth/reset-password";
--- lib/core/routes/route_generator.dart
+++ lib/core/routes/route_generator.dart
@@ -9,9 +9,12 @@ import 'package:final_route_projcet_c16/features/onbording/presentation/blocs/on
 import 'package:final_route_projcet_c16/features/onbording/presentation/blocs/onboarding_event.dart';
 import 'package:final_route_projcet_c16/features/onbording/data/onboarding_repository_impl.dart';
 import 'package:final_route_projcet_c16/features/onbording/domain/use_cases/get_onboarding_data_usecase.dart';
+import 'package:final_route_projcet_c16/features/browser/presentation/view/browse.dart';
+import 'package:final_route_projcet_c16/features/browser/presentation/view_model/bloc/browse_bloc.dart';
 import 'package:final_route_projcet_c16/features/main/main_layout.dart';
 import 'package:final_route_projcet_c16/features/update_profile/presentation/view/update_profile.dart';
-
+import '../../features/browser/presentation/view_model/bloc/browse_event.dart';
+import '../di/di.dart';
 
 abstract class RoutesManager {
   static Route<dynamic> router(RouteSettings settings) {
@@ -29,17 +32,24 @@ abstract class RoutesManager {
         return _pageRoute(const RegisterScreen());
       case AppRoutes.main:
         {
-          return CupertinoPageRoute(
-              builder: (context) => MainLayout());
+          return CupertinoPageRoute(builder: (context) => MainLayout());
         }
       case AppRoutes.updateProfile:
         {
-          return CupertinoPageRoute(
-              builder: (context) => UpdateProfile());
+          return CupertinoPageRoute(builder: (context) => UpdateProfile());
         }
-        default:
+
+    case AppRoutes.browse:
+  final genre = settings.arguments as String?; 
+  return CupertinoPageRoute(
+    builder: (_) => BlocProvider(
+      create: (_) => sl<BrowseBloc>()..add(LoadMovieEvent()),
+      child: Browse(selectedGenre: genre, showBackButton: true,), 
+    ),
+  );
+
+      default:
         return _pageRoute(const SplashScreen());
-     
     }
   }
 
